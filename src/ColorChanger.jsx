3d4b347f44469e3d4472d1a15e62719f9208656c
Rashmi <5Rashmi>
2024-08-24 17:11:59 +0530

    color picker jsx file code

--- src/ColorChanger.jsx
+++ src/ColorChanger.jsx
@@ -3,12 +3,17 @@ import React, {useState} from "react";
 function ColorChanger(){
     const [color, setColor] = useState("#FFFFFF");
 
+    function handleColorChanger(event){
+        setColor(event.target.value);
+    }
     return(
     <div className="color-picker-container">
         <h1>Color Picker</h1>
         <div className="color-display" style={{backgroundColor: color}}>
             <p>Selected Color: {color}</p>
         </div>
+        <label>Select Color: </label>
+        <input type="color" value={color} onChange={handleColorChanger}></input>
     </div>
     )
 }
